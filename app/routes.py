24cfbc8f09b6ae49c1fdf0c7d8763fd29ae26967
Paul New <novoid86@yandex.ru>
2019-08-12 21:06:16 +0300

    added forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
@@ -20,4 +21,13 @@ def index():
             'body': 'Какая гадость эта ваша заливная рыба!!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)  # TODO Шаблон
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
