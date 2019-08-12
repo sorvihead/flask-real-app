05915fcace8933172ea1b7e0b90ee975c968edd6
Paul New <novoid86@yandex.ru>
2019-08-12 21:53:58 +0300

    add models

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
