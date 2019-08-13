93783a549f514a61e6d03b93a26ad3b262984b96
Paul New <novoid86@yandex.ru>
2019-08-13 18:52:49 +0300

    Edit profile added

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DEBUG = True
+    FLASK_DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
