ee15875cb32e6a0a3bbc2c97b1bb5b9727376ccf
Paul New <novoid86@yandex.ru>
2019-08-16 21:02:03 +0300

    fixed bug in edit_profile_form

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config(object):
     FLASK_DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['novoid86@yandex.ru']
+    LOG_TO_STDOUT = int(os.environ.get('LOG_TO_STDOUT') or 0)
